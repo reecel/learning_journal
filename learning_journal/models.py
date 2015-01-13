42d9e4c60ec5fd773703fbea01b5ce9960dea214
Lesley <lesley.d.reece@gmail.com>
2015-01-12 20:54:31 -0800

    committing new entries model

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -3,6 +3,9 @@ from sqlalchemy import (
     Index,
     Integer,
     Text,
+    Unicode,
+    UnicodeText,
+    DateTime,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -25,3 +28,31 @@ class MyModel(Base):
     value = Column(Integer)
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+class entries(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode(255), unique=True, nullable=False)
+    body = Column(UnicodeText)
+    created = Column(DateTime, default=None)
+    edited = Column(DateTime, default=None)
+
+    def createdtime(created=None):
+        if created is None:
+	    created = datetime.datetime.now()
+    def editedtime(edited=None):
+        if edited is None:
+	    edited = datetime.datetime.now()
+
+
+
+    @classmethod
+    def all(rows):
+        q1 = session.query(rows).all()
+        return q1
+
+    @classmethod
+    def by_id(rows,id_number):
+        q2 = session.query(rows).filter_by(id=id_number)
+        return q2
+
